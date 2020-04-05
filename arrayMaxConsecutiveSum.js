b7d619c5472a0712aac0c969b653114a60421580
Козлов Павел Олегович <kozlov_po@astralnalog.ru>
2020-04-05 16:13:08 +0300

    Fixed an error in calculations

--- arrayMaxConsecutiveSum.js
+++ arrayMaxConsecutiveSum.js
@@ -1,11 +1,14 @@
 function maxSum(arr, k) {
     let max = 0;
+    let sum = 0;
     for (let i = 0; i< k; i++) {
-        max+=arr[i]
+        sum+=arr[i]
     }
-    for (let i = k; i< arr.length-k+1; i++) {
-        let sum = arr[k] - arr[i-k] + arr[i]
+    max = sum
+    for (let i = k; i< arr.length; i++) {
+        sum = sum - arr[i-k] + arr[i]
         max = Math.max(max, sum)
     }
     return max
 }
+
